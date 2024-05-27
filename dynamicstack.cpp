3808ab91754711fb9c9a3c12c7cddb48e73fa771
Carl Mariño <97602174+carlmarinyo@users.noreply.github.com>
2024-05-27 22:54:58 +0800

    Update dynamicstack.cpp

--- dynamicstack.cpp
+++ dynamicstack.cpp
@@ -3,33 +3,33 @@
 using namespace std;
 
 void DynamicStack::push(int num) {
-	StackNode *newNode;
-	newNode = new StackNode;
-	newNode->value = num;
+	StackNode *newNode; //creates a pointer
+	newNode = new StackNode; // pointer uses new to create a node
+	newNode->value = num; //assign the value of the node to the given num
 	
 	if (isEmpty()) {
-		top = newNode;
-		top->next = NULL;
+		top = newNode;   // if stack is empty during the node creation, the node will be pointed as head
+		top->next = NULL; 
 	}
 	
 	else {
-		newNode->next = top;
-		top = newNode;
+		newNode->next = top; //if there are already nodes existing, it will point to the previous top as the movements goes from top to bottom
+		top = newNode; //the new node will now be pointed as the top
 	}
 }
 
 void DynamicStack::pop(int &num) {
-	StackNode *temp;
+	StackNode *temp; 
 	
-	num = top->value;
-	temp = top->next;
-	delete top;
-	top = temp;
+	num = top->value; //gets the current value of node to be passed by reference
+	temp = top->next; //temp pointer points to the next node (the one before the current)
+	delete top; 
+	top = temp; //points top to what temp is pointing (the node before the current deleted)
 }
 
 bool DynamicStack::isEmpty() {
 	bool status;
-	if (!top) status = true;
+	if (!top) status = true; //if top is zerovalue, then stack is empty
 	else status = false;
 	return status;
 }
